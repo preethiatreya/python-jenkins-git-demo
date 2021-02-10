1932d4de9f7ae2d414d33433c55255c66f96eea7
Radha Krishna Preethi Atreyapurapu <radha.krishna.preethi.atreyapurapu@ibm.com>
2021-02-10 14:45:17 +0530

    first commit

---
+++ listdemo.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 12:23:59 2021
+
+@author: RadhaKrishnaPreethiA
+"""
+
+a= [2,4,5]
+b= [6,7,9]
+print(a+b)
+print(a*2)
+print(5 in a)
+
+for x in a:
+    print(x)
